1d8b99a99eba109b730fa180451af7d32e6e7c05
Abraão Azevedo <az.abraao@gmail.com>
2020-08-11 20:52:12 -0300

    use sqlite to list stocks

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,6 +1,11 @@
+const StockDao = require("../infra/stock-dao.js");
+console.log(StockDao);
+
 module.exports = (app) => {
-    app.get('/', (req, resp) => {
-        resp.send(`
+  const db = require("../../config/database");
+
+  app.get("/", (req, resp) => {
+    resp.send(`
             <html>
                 <head>
                     <title>Stocks Motion</title>
@@ -10,27 +15,23 @@ module.exports = (app) => {
                 </body>
             </html>
         `);
-    });
+  });
 
-    app.get('/stocks', (req, resp) => {
-        resp.marko(
-            require('../views/portifolio/portifolio.marko'),
-            {
-                stocks: [
-                    {
-                        code: 'VVAR',
-                        name:"asd",
-                        amount: 600,
-                        price: 6000,
-                    },
-                    {
-                        code: 'VVAR',
-                        name:"bbbbb",
-                        amount: 600,
-                        price: 6000,
-                    }
-                ]
-            }
-        )
-    });
-}
+  app.get("/stocks", (req, resp) => {
+    const dao = new StockDao(db);
+
+    dao
+      .list()
+      .then((stocks) => {
+        resp.marko(require("../views/portifolio/portifolio.marko"), {
+          stocks,
+        });
+      })
+      .catch(console.error);
+  });
+    
+   
+    app.get("/stocks/buy", (req, resp) =>
+      resp.marko(require("../views/portifolio/new.marko"))
+    );
+};
